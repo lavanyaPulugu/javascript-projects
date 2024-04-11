57fa2a01005812d5edd86c5afc51a500711514f5
lavanyapulugu <lavanya415@gmail.com>
2024-04-11 10:33:21 +0530

    date object

---
+++ dates/index.js
@@ -0,0 +1,23 @@
+const date = new Date();
+// get date
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDate();
+// const hour = date.getHours();
+// const minute = date.getMinutes();
+// const seconds = date.getSeconds();
+// const dayOfWeek = date.getDay();
+
+// console.log(year);
+// console.log(month);
+// console.log(day);
+// console.log(hour + ":" + minute + ":" + seconds);
+// console.log(dayOfWeek);
+
+//set date
+
+date.setFullYear(2023);
+date.setMonth(5);
+date.setDate(1);
+
+console.log(date);
