6e3749b08b29dcc12be4c90580d4a9f5162ff8a0
lavanyapulugu <lavanya415@gmail.com>
2024-04-15 06:54:02 +0530

    add and change html

---
+++ dom navigation/index.js
@@ -0,0 +1,7 @@
+// DOM (Document Object Model) navigation in JavaScript refers to the process of traversing and manipulating the structure of HTML documents using JavaScript.
+
+const element = document.getElementById("fruits");
+
+const firstchild = element.firstElementChild();
+
+firstchild.style.backgroundColor = "yellow";
